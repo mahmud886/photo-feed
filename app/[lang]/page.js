01a2cd1f709fe0dca675afdfd87e9aecb35a834c
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2024-04-01 01:42:17 +0600

    first commit

---
+++ app/[lang]/page.js
@@ -0,0 +1,9 @@
+import { getDictionary } from "./dictionaries";
+
+export default async function Home({params: {lang}}) {
+  const dictionary = await getDictionary(lang);
+  return (
+    <div>{dictionary.followers}</div>
+  );
+}
+
